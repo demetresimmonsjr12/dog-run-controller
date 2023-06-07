fd533a089c1f46c4abcab89af9a718377a270d2b
demetresimmonsjr12 <135878578+demetresimmonsjr12@users.noreply.github.com>
2023-06-07 15:15:18 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+radio.setGroup(3)
+radio.setFrequencyBand(33)
+basic.showIcon(IconNames.Happy)
+music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
 basic.forever(function () {
 	
 })
