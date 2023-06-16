bb36c5196e28276ee01b245cc0206373f277dbbf
demetresimmonsjr12 <135878578+demetresimmonsjr12@users.noreply.github.com>
2023-06-16 14:47:35 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+input.onGesture(Gesture.Shake, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    radio.sendNumber(5)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(4)
+})
 radio.setGroup(3)
 radio.setFrequencyBand(33)
 basic.showIcon(IconNames.Happy)
 music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
-basic.forever(function () {
-	
-})
